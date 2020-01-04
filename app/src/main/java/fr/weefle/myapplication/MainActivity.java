3a80d8c57a9f0e63e5aa4becf909cd689af4c823
Weefle <mr.weefle@gmail.com>
2020-01-04 10:40:43 +0100

    Initial commit

---
+++ app/src/main/java/fr/weefle/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package fr.weefle.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Fragment fragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //startActivity(new Intent(getApplicationContext(), ReceiverFragment.class));
+                    fragment = new ReceiverFragment();
+                    break;
+               /* case R.id.navigation_dashboard:
+
+                    break;
+                case R.id.navigation_notifications:
+
+                    break;*/
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/fr/weefle/myapplication/ReceiverFragment.java
@@ -0,0 +1,79 @@
+package fr.weefle.myapplication;
+
+import androidx.fragment.app.Fragment;
+
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiverFragment extends Fragment {
+
+    private static final Uri SMS_URI_INBOX = Uri.parse("content://sms/inbox");
+
+    private static final Uri SMS_URI_ALL = Uri.parse("content://sms/");
+
+    private static final Uri SMS_URI_OUTBOX = Uri.parse("content://sms/sent");
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        final View rootView = inflater.inflate(R.layout.fragment_receiver, container, false);
+
+        final ListView list = (ListView) rootView.findViewById(android.R.id.list);
+        final List<String> messages = retrieveMessages(getActivity().getContentResolver());
+
+        if (messages != null)
+        {
+            list.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, messages));
+        }
+
+        return rootView;
+    }
+
+    private List<String> retrieveMessages(ContentResolver contentResolver)
+    {
+        final List<String> messages = new ArrayList<>();
+        final Cursor cursor = contentResolver.query(ReceiverFragment.SMS_URI_INBOX, null, null, null, null);
+
+        if (cursor == null)
+        {
+            Log.e("retrieveMessages", "Cannot retrieve the messages");
+            return null;
+        }
+
+        if (cursor.moveToFirst() == true)
+        {
+            do
+            {
+                final String address = cursor.getString(cursor.getColumnIndexOrThrow("address"));
+                final String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+
+                messages.add(address + " - " + body);
+
+                Log.d("retrieveContacts", "The message with from + '" + address + "' with the body '" + body + "' has been retrieved");
+            }
+            while (cursor.moveToNext() == true);
+        }
+
+        if (cursor.isClosed() == false)
+        {
+            cursor.close();
+        }
+
+        return messages;
+    }
+}
